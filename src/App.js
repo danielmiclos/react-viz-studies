1e67278d44243549bb03327e524c652a5bfed999
danielmiclos2 <miclos.o2filmes@gmail.com>
2019-04-09 19:11:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as crossfilter from 'crossfilter2';
+import dc from 'dc';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {data:[]};
+  }
+
+  componentDidMount() {
+    fetch("./data/dailies.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log('success');
+          //console.log(result);
+          this.setState({data: result});
+          this.makeData(result);
+        },
+        (error) => {
+          console.log('something went wrong');
+          console.log(error);
+        }
+      )
+  }
+
+  makeData = (data) => {
+      const  dailies = crossfilter(data);
+
+      const jobDimension = dailies.dimension(d => d.job_name);
+      const jobGroup = jobDimension.group();
+
+      const dayDimension = dailies.dimension(d => d.daily_date);
+      const dayGroup = dayDimension.group();
+
+      const dataLenghtDimension = dailies.dimension(d => d.data_length);
+
+      console.log(jobDimension.filterAll().top(Infinity));
+      console.log(jobGroup.top(Infinity));
+
+      const menu = dc.selectMenu(this.menu)
+        .dimension(jobDimension)
+        .group(jobGroup);
+
+
+
+      dc.renderAll();
+  };
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <h1>{React.version}</h1>
+        <div className="list" ref={(ref) => this.menu = ref}>
+          <h2>Menu</h2>
+        </div>
+        <div ref={(ref) => this.barDiarias = ref}>
+          <h2>Diarias</h2>
+        </div>
       </div>
     );
   }
